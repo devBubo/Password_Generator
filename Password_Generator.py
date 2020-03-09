d119af543aa7b9b6e2cd5377fddc9d163232ffb2
devBubo <bubak.filip@gmail.com>
2020-03-09 21:36:44 +0100

    alphabet and numbers

--- Password_Generator.py
+++ Password_Generator.py
@@ -1,1 +1,11 @@
-len=input('Enter a lenght of password')
+from random import randint
+lenght=int(input('Enter a lenght of password: '))
+alphabet = 'qrupwtieyoaxfbjmlszdcgvhnk'
+numbers='0123456789'
+symbols=',.-§?:_"!(;!@^#$%*/-)¨,.'
+for char in range(lenght):
+    n=randint(0,2)
+    if n==0:
+        print(alphabet[randint(0,len(alphabet))-1],end='')
+    elif n==1:
+        print(numbers[randint(0,len(numbers)-1)],end='')
