6ae9165af9734f9656986dce74aed3f80d9d7e98
devBubo <bubak.filip@gmail.com>
2020-03-09 22:21:07 +0100

    completed

--- Password_Generator.py
+++ Password_Generator.py
@@ -1,13 +1,16 @@
 from random import randint
 lenght=int(input('Enter a lenght of password: '))
 alphabet = 'qrupwtieyoaxfbjmlszdcgvhnk'
+alphabetcap = alphabet.upper()
 numbers='0123456789'
 symbols=',.-§?:_"!(;!@^#$%*/-)¨,.'
 for char in range(lenght):
-    n=randint(0,2)
+    n=randint(0,3)
     if n==0:
         print(alphabet[randint(0,len(alphabet))-1],end='')
     elif n==1:
-        print(numbers[randint(0,len(numbers)-1)],end='')
+        print(alphabetcap[randint(0,len(alphabetcap)-1)],end='')
     elif n==2:
+        print(numbers[randint(0,len(numbers)-1)],end='')
+    elif n==3:
         print(symbols[randint(0, len(symbols) - 1)], end='')
